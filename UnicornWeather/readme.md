1ad8b496645e21573619aa9c7ee2f2f4ae2b4d34
Craig // Perkybutts <craig@cra.io>
2016-02-11 21:58:37 +0000

    UnicornWeather
    Added UnicornWeather files

---
+++ UnicornWeather/readme.md
@@ -0,0 +1,81 @@
+UnicornWeather
+=============
+This python script will display the current weather status by means of an icon on your UnicornHAT, a Raspberry Pi attatchment. This information is pulled from [Forecast.io](https://forecast.io/).
+
+Installation
+-------
+To install the UnicornHAT Library you'll want to use the following commands (Check [here](https://github.com/pimoroni/unicorn-hat) if outdated):
+
+```bash
+\curl -sS get.pimoroni.com/unicornhat | bash
+```
+You'll then want to install ZeevG's python wrapper for the forecast.io API (Check [here](https://github.com/ZeevG/python-forecast.io) if outdated):
+
+````bash
+install pip install python-forecastio
+````
+
+
+Usage
+-------
+(Just as a note before we get started, check out [ZeevG's ReadMe](https://github.com/ZeevG/python-forecast.io) here for a more detailed explination of how their project is used and how this integates with UnicornWeather)
+
+---
+
+In order to use this script you will need a free [Forecast.io Developer account](https://developer.forecast.io/) and the longitude and latitude of the location you are interested in gathering weather information.
+
+You can find the longitude and latiitude from the Forecast.io website by seaching for your location and taking note of the URL.
+
+For Example:
+
+The URL for Glasgow, Scotland would be `https://forecast.io/#/f/55.8578,-4.2425`
+
+The part we are interested in would be `55.8578,-4.2425`
+
+Where `55.8578` is the longitude and `-4.2425` is the latitidue. These values are set to the `lat` and `lng` variables in the script respectfuly.
+
+
+---
+
+The images called by this script are just regular 8 pixels by 8 pixels, you can modify these to display anything you'd like. 
+
+
+Additional notes
+-------
+Although this is commented within the script I've added this here just for sake of clarity.
+
+You can change the brightness by modifying the numbers in the `unicorn.brightness()` below to your needs.
+
+````python
+  # set pixel brightness based on time of day
+  def setBrightness(currenttime):
+    currenthour = currenttime.tm_hour
+    # if it's between 12 pm and 9 pm,
+    # use lower brightness value
+    if(currenthour < 12 or currenthour > 21):
+      unicorn.brightness(0.05)
+    else:
+      unicorn.brightness(0.3)
+  # set brightness for time of day
+  setBrightness(currenttime)
+````
+
+---
+
+Another note, a free Forecast.io developer account will only allow for 1000 calls per day without cost. In order to stay under this limit it is important to make sure the numbers given in `time.sleep()` will not cause the script to call for information too often.
+
+Currently the script is set to call information every 120 seconds or every two minutes, this means the script will make a total of 720 calls per day falling well below the free 1000 limit.
+
+Credits
+-------
+[Pimoroni](https://pimoroni.com/) for writing the [Library for UnicornHAT](https://github.com/pimoroni/unicorn-hat) and providing excelent examples on how to use this awesome little device!
+
+GitHub user [ZeevG](https://github.com/ZeevG/) for writing the](https://github.com/ZeevG/python-forecast.io)
+
+[Forecast.io](http://forecast.io) for providing an easy to use API and for having a pretty cool website too! 
+
+I guess some to [me](https://github.com/Craio/) but really all the hard work was done by the people above!
+
+Licence
+-------
+I'm not super keen on crazy licence restrictions and limitations, you can do whatever you want with this project, go nuts!
---
+++ UnicornWeather/unicornweather.py
@@ -0,0 +1,173 @@
+##
+##				| | | |     (_)                    | |  | |          | | | |	
+##				| | | |_ __  _  ___ ___  _ __ _ __ | |  | | ___  __ _| |_| |__   ___ _ __ 
+##				| | | | '_ \| |/ __/ _ \| '__| '_ \| |/\| |/ _ \/ _` | __| '_ \ / _ \ '__|
+##				| |_| | | | | | (_| (_) | |  | | | \  /\  /  __/ (_| | |_| | | |  __/ | 
+##	 			 \___/|_| |_|_|\___\___/|_|  |_| |_|\/  \/ \___|\__,_|\__|_| |_|\___|_|
+## 
+##	Code by:
+##			https://github.com/craio/
+##			https://github.com/pimoroni/unicorn-hat/		
+##			https://github.com/ZeevG/python-forecast.io/
+##
+##	Check readme.md at https://github.com/Craio/RaspberryPi/blob/master/UnicornWeather/readme.md
+##	for information on how to use this. Forgive my bad code please!
+
+
+#!/usr/bin/env python
+
+from random import randint
+from PIL import Image
+import unicornhat as unicorn
+import forecastio, time, colorsys, signal, numpy
+
+
+# set forecast.io api
+
+#insert your own api key in these brackets eg "abcdef1234567890"
+api_key = "" 
+# set lng and lat of location (just the numbers) i.e
+# lat = 55.8578
+# lng = -4.2425
+lat = 
+lng = 
+
+#rotate display to show pngs with direction of the pi power cable facing 'up' to space
+unicorn.rotation(270)
+
+imgcloud = Image.open('img/weather_cloud.png')
+imgsun = Image.open('img/weather_sun.png')
+imgmoon = Image.open('img/weather_moon.png')
+imgrain = Image.open('img/weather_rain.png')
+imgsnow = Image.open('img/weather_snow.png') # change this png to display snowflake - one day I'll do this
+imgwind = Image.open('img/weather_wind-line.png') # this can be swapped for 'weather_wind-turbine' if desired
+ 
+def weatherloop():
+
+  # define current time
+  currenttime = time.localtime()
+
+  # set pixel brightness based on time of day
+  def setBrightness(currenttime):
+    currenthour = currenttime.tm_hour
+    # if it's between 12pm and 9pm,
+    # use lower brightness value
+    if(currenthour < 12 or currenthour > 21):
+      unicorn.brightness(0.05)
+    else:
+      unicorn.brightness(0.3)
+  # set brightness for time of day
+  setBrightness(currenttime)
+
+  # define forecast function (shortens the need to type long name)
+  forecast = forecastio.load_forecast(api_key, lat, lng)
+
+  # defines current weather function and prints icon conditions and temperature to terminal
+  current_weather = forecast.currently()
+  print current_weather.icon
+  print current_weather.temperature
+
+  # if weather is cloudy, print cloud icon
+  if current_weather.icon in ['partly-cloudy-day','partly-cloudy-night']:
+      for o_x in range(int(imgcloud.size[0]/8)):
+	for o_y in range(int(imgcloud.size[1]/8)):
+
+		for x in range(8):
+			for y in range(8):
+				pixel = imgcloud.getpixel(((o_x*8)+y,(o_y*8)+x))
+                                # uncoment line below to enable printing pixel information to console window
+				#print(pixel)
+				r, g, b = int(pixel[0]),int(pixel[1]),int(pixel[2])
+				unicorn.set_pixel(x, y, r, g, b)
+                unicorn.show()
+                time.sleep(120)
+                unicorn.clear()
+      
+  # if weather is sunny, print sun icon
+  if current_weather.icon in ['clear-day']:
+      for o_x in range(int(imgsun.size[0]/8)):
+	for o_y in range(int(imgsun.size[1]/8)):
+
+		for x in range(8):
+			for y in range(8):
+				pixel = imgsun.getpixel(((o_x*8)+y,(o_y*8)+x))
+                                # uncoment line below to enable printing pixel information to console window
+				#print(pixel)
+				r, g, b = int(pixel[0]),int(pixel[1]),int(pixel[2])
+                                unicorn.set_pixel(x, y, r, g, b)
+                unicorn.show()
+                time.sleep(120)
+                unicorn.clear()
+
+
+  # if weather is clear at night, print moon icon
+  if current_weather.icon in ['clear-night']:
+      for o_x in range(int(imgmoon.size[0]/8)):
+	for o_y in range(int(imgmoon.size[1]/8)):
+
+		for x in range(8):
+			for y in range(8):
+				pixel = imgmoon.getpixel(((o_x*8)+y,(o_y*8)+x))
+                                # uncoment line below to enable printing pixel information to console window
+				#print(pixel)
+				r, g, b = int(pixel[0]),int(pixel[1]),int(pixel[2])
+				unicorn.set_pixel(x, y, r, g, b)
+                unicorn.show()
+                time.sleep(120)
+                unicorn.clear()
+
+
+  # if weather is rainy, print rain animation
+  if current_weather.icon in ['rain']:
+      for o_x in range(int(imgrain.size[0]/8)):
+	for o_y in range(int(imgrain.size[1]/8)):
+
+		for x in range(8):
+			for y in range(8):
+				pixel = imgrain.getpixel(((o_x*8)+y,(o_y*8)+x))
+                                # uncoment line below to enable printing pixel information to console window
+				#print(pixel)
+				r, g, b = int(pixel[0]),int(pixel[1]),int(pixel[2])
+				unicorn.set_pixel(x, y, r, g, b)
+                unicorn.show()
+                time.sleep(120)
+                unicorn.clear()
+
+
+  # if weather is snowy, print snow animation
+  if current_weather.icon in ['snow']:
+      for o_x in range(int(imgsnow.size[0]/8)):
+	for o_y in range(int(imgsnow.size[1]/8)):
+
+		for x in range(8):
+			for y in range(8):
+				pixel = imgsnow.getpixel(((o_x*8)+y,(o_y*8)+x))
+                                # uncoment line below to enable printing pixel information to console window
+				#print(pixel)
+				r, g, b = int(pixel[0]),int(pixel[1]),int(pixel[2])
+				unicorn.set_pixel(x, y, r, g, b)
+                unicorn.show()
+                time.sleep(120)
+                unicorn.clear()
+
+
+  # if weather is windy, print wind icon
+  if current_weather.icon in ['wind']:
+      for o_x in range(int(imgwind.size[0]/8)):
+	for o_y in range(int(imgwind.size[1]/8)):
+
+                for x in range(8):
+                        for y in range(8):
+                                pixel = imgwind.getpixel(((o_x*8)+y,(o_y*8)+x))
+                                # uncoment line below to enable printing pixel information to console window
+                                #print(pixel)
+                                r, g, b = int(pixel[0]),int(pixel[1]),int(pixel[2])
+                                unicorn.set_pixel(x, y, r, g, b)
+                unicorn.show()
+                time.sleep(120)
+                unicorn.clear()
+
+
+while True:
+  weatherloop()
+
